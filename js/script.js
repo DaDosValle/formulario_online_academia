6693f3818336f97ea216f3f956ed9a6ff06cb4c6
FERNANDO MENDONCA DO VALLE <fsf.dovalle@gmail.com>
2025-09-14 17:53:06 -0300

    add url render

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Determina a URL do servidor de acordo com o ambiente
     const BASE_URL = window.location.hostname.includes("localhost")
         ? "http://127.0.0.1:5000"
-        : "https://SEU_APP_RENDER.onrender.com"; // <--- substitua pela sua URL do Render
+        : "https://formulario-online-academia.onrender.com"; // <--- substitua pela sua URL do Render
 
     btnSalvar.addEventListener("click", async function (event) {
         event.preventDefault();
